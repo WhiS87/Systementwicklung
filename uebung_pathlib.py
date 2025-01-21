8d8fb5e0ffccbd1750641351d4f6ac6f55c48e57
WhiS <contact@manuel-hartmann.it>
2025-01-21 15:54:18 +0100

    Implement dynamic file creation with user-defined name and type

--- uebung_pathlib.py
+++ uebung_pathlib.py
@@ -1,4 +1,12 @@
 from pathlib import Path
 
-new_file = Path("C:\\Users\\HartmanM\\OneDrive - Berufsförderungswerk Köln gGmbH\\Dokumente\\Testdaten\\pathlib\\data.txt")
-new_file.touch()
+# new_file = Path("C:\\Users\\HartmanM\\OneDrive - Berufsförderungswerk Köln gGmbH\\Dokumente\\Testdaten\\pathlib\\data.txt")
+# new_file.touch()
+
+home = Path ("C:\\Users\\HartmanM\\OneDrive - Berufsförderungswerk Köln gGmbH\\Dokumente\\Testdaten\\pathlib")
+
+name = input("Wie soll die datei heißen?\n")
+datentyp = input("was soll sie für ein datentypsein? (.txt,.html,.pdf,.json,.py usw.)\n")
+dateiname = name + "." + datentyp
+Path(home, dateiname).touch(mode=0o777, exist_ok=True)
+print(f'Die Datei "{dateiname}" wurde in "{home}" erstellt!')
